1d04f11e7c25b1b5e431c177830890b2f53e812e
swimmerka-coder <69499699+swimmerka-coder@users.noreply.github.com>
2020-08-20 09:13:47 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        variableWidth: true,
+    });
+});
